d2763fb2c393fcc00ca3619cb77936bf0d8a575b
Simon Luder <simon.luder@students.fhnw.ch>
2023-12-08 19:17:26 +0100

    Update reshape_dataset.py

--- reshape_dataset.py
+++ reshape_dataset.py
@@ -1,12 +1,27 @@
 import os
 import cv2
 import argparse
-from tqdm import tqdm
 import pandas as pd
-import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 
 def resize_images(source_folder, dest_folder, size=(128, 128)):
+    """
+    Resizes images from a source folder and saves them to a destination folder.
+
+    This function reads each image file (with .jpg or .png extension) from the source folder,
+    resizes it to the specified size using area interpolation, and writes the resized image
+    to the destination folder. If the destination folder does not exist, it is created.
+
+    Args:
+        source_folder (str): Path to the source folder containing the images.
+        dest_folder (str): Path to the destination folder where resized images will be saved.
+        size (tuple, optional): Desired size of the output images as a tuple (width, height). 
+            Default is (128, 128).
+
+    Returns:
+        None
+    """
     if not os.path.exists(os.path.join(dest_folder, "images")):
         os.makedirs(os.path.join(dest_folder, "images"))
 
@@ -21,18 +36,17 @@ def replace_image_paths(df, column, new_dir):
     """
     Replace the directory part of the file paths in a DataFrame but keep the filenames.
 
-    Parameters:
-    df (DataFrame): DataFrame containing the file paths.
-    column (str): Column name containing the file paths.
-    new_dir (str): New directory to replace the old one.
+    Args:
+        df (DataFrame): DataFrame containing the file paths.
+        column (str): Column name containing the file paths.
+        new_dir (str): New directory to replace the old one.
 
     Returns:
-    DataFrame: DataFrame with updated file paths.
+        DataFrame: DataFrame with updated file paths.
     """
     df[column] = df[column].apply(lambda x: os.path.join(new_dir, os.path.basename(x)))
     df[column] = df[column].str.replace("\\", "/")
     return df
-
 
 
 if __name__ == "__main__":
@@ -40,18 +54,23 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--source', type=str, default="./data/train/", help="Source directory")
     parser.add_argument('--destination', type=str, default=f"./data/train32")
+    parser.add_argument('--custom_destination_path', type=str, default=None)
     parser.add_argument('--size', type=int, default=32, help="Image size for square proportions")
-
-  
     args = parser.parse_args()
 
     if not os.path.exists(args.destination):
         os.makedirs(args.destination)
 
     # update and save csv
-    df = pd.read_csv(os.path.join(args.source, "labels.csv"))
-    df = replace_image_paths(df, "destination", args.destination)
-    df = replace_image_paths(df, "file", os.path.join(args.destination, "images"))
-    df.to_csv(os.path.join(args.destination, "labels.csv"))
+    if args.custom_destination_path is not None:
+        destination_path = args.custom_destination_path
+    else:
+        destination_path = args.destination
 
+    df = pd.read_csv(os.path.join(args.source, "labels.csv"))
+    df = replace_image_paths(df, "destination", destination_path)
+    df = replace_image_paths(df, "file", os.path.join(destination_path, "images"))
+    df.to_csv(os.path.join(args.destination, "labels.csv"), index=False)
+
+    # reshape and move images
     resize_images(source_folder=args.source, dest_folder=args.destination, size=(args.size, args.size))
