bafbff9a53e38901095743494d77f31c39a860b6
Simon Luder <simon.luder@students.fhnw.ch>
2024-01-10 10:57:13 +0100

    Create subsample_dataset.py

---
+++ utils/dataset/subsample_dataset.py
@@ -0,0 +1,36 @@
+import os
+import git
+import argparse
+import pandas as pd
+
+def get_git_root(path):
+
+        git_repo = git.Repo(path, search_parent_directories=True)
+        git_root = git_repo.git.rev_parse("--show-toplevel")
+        return git_root
+
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--source', type=str, default="data/train256/", help="Source directory")
+    parser.add_argument('--destination', type=str, default=None)
+    parser.add_argument('--n', type=int, default=100, help="Nr of samples per shape")
+    args = parser.parse_args()
+
+
+    git_root = get_git_root(".")
+
+    source = args.source
+    destination = args.destination
+
+    if destination is None:
+        destination = source
+
+    if not os.path.exists(os.path.join(git_root, destination)):
+        os.makedirs(os.path.join(git_root, destination))
+
+    # update and save csv
+    df = pd.read_csv(os.path.join(git_root, source, "labels.csv"))
+    df_sub = df.groupby("shape_name").head(args.n)
+    df_sub.to_csv(os.path.join(git_root, destination, f"labels_sub{args.n}.csv"), index=False)
