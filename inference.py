8f156b8d3e070d25e7c2804ab1e16fe0319305b3
Simon Luder <simon.luder@students.fhnw.ch>
2024-02-01 19:42:53 +0100

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,18 +1,11 @@
 
-import os
 import json
 import argparse
-import pandas as pd
-from tqdm import tqdm
-from pathlib import Path
-from PIL import Image
 
 import torch
 import clip
 
-from utils.metrics import iou_pytorch, center_distance_pytorch
 from utils.train_test_utils import initialize_model_weights, save_images_batch
-from utils.wandb import WandbManager, wandb_image
 from model.UNet import UNet
 from model.ddpm import Diffusion as DDPMDiffusion
 
@@ -33,28 +26,33 @@ def inference(run_name, epoch, condition):
     # load model
     if args.model == "unet_base":
         model = UNet(image_size=args.image_size, 
+                    in_channel=args.image_channels,
+                    out_channel=args.image_channels,
                     cfg_encoding=args.cfg_encoding,
                     num_classes=args.num_classes,
                     cfg_model_name=args.encoder_model,
                     device=args.device,
-                    act=args.act)
+                    act=args.act,
+                    ).to(args.device)
     else:
         print(f'"{args.model}" model not implemented')
+
 
     initialize_model_weights(model=model, weight_path=f"runs/{run_name}/models/{epoch}/model.pt", device=args.device)
     model.to(args.device).eval()
 
     # set diffusion model
     diffusion = DDPMDiffusion(img_size=args.image_size, 
+                              img_channels=args.image_channels,
                               noise_schedule=args.noise_schedule, 
                               beta_start=args.beta_start, 
                               beta_end=args.beta_end,
                               s=args.s,
                               device=args.device,
                           )
-
+    
     # apply clip tokenization
-    if args.cfg_encoding == "clip":
+    if args.cfg_encoding == "clip_text":
         condition = clip.tokenize(condition)
 
     condition = condition.to(args.device)
@@ -68,8 +66,8 @@ def inference(run_name, epoch, condition):
 
 if __name__ == "__main__":
 
-    run_name = "2D_GeoShape_32_linear_clip_1701625192"
+    run_name = "2D_GeoShape_64_linear_clip_text_1705056262"
     epoch = "latest"
-    condition = "star with radius 51 aspect ratio 1.7 rotation -24 degrees located at (76, 92)"
+    condition = "triangle with radius 84 aspect ratio 1.7 rotation -24 degrees located at (76, 92)"
     inference(run_name, epoch, condition)
 
